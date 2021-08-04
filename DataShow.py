133e49b8c0b0d96c9452079dcb83df65e6488551
zovgu <ovgu1226@hotmail.com>
2021-08-05 02:39:03 +0300

    Update DataShow.py

--- DataShow.py
+++ DataShow.py
@@ -2,29 +2,14 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import os
+import cv2
+import argparse
  
 
 images = np.load(r'C:\Users\90534\Desktop\YBitirme\Cancer-Detection-and-segmentation-over-Histological-Images\imageExamples.npy')
 masks=np.load(r'C:\Users\90534\Desktop\YBitirme\Cancer-Detection-and-segmentation-over-Histological-Images\maskExamples.npy')
 images=images.astype(int)
-masks=masks.astype(int)
-
-
-"Data seperation (Seperated into 5 instances"
-
-# imageExampels=[]
-# maskExampels=[]
-
-# for i in range(5):
-#     imageExampels.append(images[i])
-#     maskExampels.append(masks[i])
-
-# imageExampels=np.array(imageExampels)
-# maskExampels=np.array(maskExampels)
-# np.save('imageExamples', imageExampels)
-# np.save('maskExamples', maskExampels)
-
-
+masks=masks.astype(float)
 
 "Plotting over a graph"
 #plt.imshow(images[0])
@@ -32,11 +17,13 @@ masks=masks.astype(int)
 #plt(images[0])
 #plt(masks[0])
 
-r1,g1, b1, r2, g2, b2 = np.split(masks[0], 6, axis=2)
-m1 = np.concatenate((r1,g1,b1), axis=2)
-m2 = np.concatenate((r2,g2,b2), axis=2)
-r3,g3,b3 = np.split(images[0],3, axis=2)
-masked = np.concatenate((r3*r2*r1, g3*g2*g1, b3*g3*g1),axis=2)
+npp, inf, stc, dc, epi, bg  = np.split(masks[0], 6, axis=2) #256x256
+m1 = np.concatenate((npp,npp,npp), axis=2)
+m2 = np.concatenate((inf, inf,inf), axis=2)
+m3 = np.concatenate((stc,stc,stc), axis=2)
+m4 = np.concatenate((dc,dc,dc), axis=2)
+m5 = np.concatenate((epi,epi,epi), axis=2)
+m6 = np.concatenate((bg,bg,bg), axis=2)
 plt.imshow(masked)
 plt.show()
 
@@ -50,7 +37,3 @@ plt.show()
     
 
 "Check how is looks like "
-
-plt.show()
-
-"PS. Izbanda kodlama queyfie"
