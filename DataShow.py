7d42b96bd08ad73a6c48390dd69d7704275851b8
zovgu <ovgu1226@hotmail.com>
2021-08-05 01:06:11 +0300

    Update DataShow.py

--- DataShow.py
+++ DataShow.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import os
  
 
-images = np.load(r'D:\Software Senior Project[DATA]\fold_1\Fold 1\images\fold1\images.npy')
-masks=np.load(r'D:\Software Senior Project[DATA]\fold_1\Fold 1\masks\fold1\masks.npy')
+images = np.load(r'C:\Users\90534\Desktop\YBitirme\Cancer-Detection-and-segmentation-over-Histological-Images\imageExamples.npy')
+masks=np.load(r'C:\Users\90534\Desktop\YBitirme\Cancer-Detection-and-segmentation-over-Histological-Images\maskExamples.npy')
 images=images.astype(int)
 masks=masks.astype(int)
 
@@ -27,20 +27,26 @@ masks=masks.astype(int)
 
 
 "Plotting over a graph"
-# plt.imshow(images[0])
-# plt.imshow(masks[0])
-# plt(images[0])
-# plt(masks[0])
+#plt.imshow(images[0])
+#plt.imshow(masks[0])
+#plt(images[0])
+#plt(masks[0])
 
+r1,g1, b1, r2, g2, b2 = np.split(masks[0], 6, axis=2)
+m1 = np.concatenate((r1,g1,b1), axis=2)
+m2 = np.concatenate((r2,g2,b2), axis=2)
+r3,g3,b3 = np.split(images[0],3, axis=2)
+masked = np.concatenate((r3*r2*r1, g3*g2*g1, b3*g3*g1),axis=2)
+plt.imshow(masked)
+plt.show()
 
 "Saving the images whichs are overplotted"
 
+#for i in range (1):
 
-# for i in range (1):
-#     plt.imshow(images[i])
-#     plt.imshow(masks[i])
-#     imagename='images'+str(i)+'.png'
-#     plt.savefig(imagename)
+
+    # imagename='images'+str(i)+'.png'
+    # plt.savefig(imagename)
     
 
 "Check how is looks like "
