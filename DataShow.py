b6d143bfdee005b785cd39fdb5c3485d541d491a
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2021-08-04 23:19:56 +0300

    Updated

--- DataShow.py
+++ DataShow.py
@@ -4,29 +4,37 @@ import matplotlib.pyplot as plt
 import os
  
 
-images = np.load(r'Images\images.npy')
-masks=np.load(r'Masks\masks.npy')
+images = np.load(r'C:\Users\ugur_\OneDrive\Masaüstü\Projects\Cancer-Detection-and-segmentation-over-Histological-Images\Cancer-Detection-and-segmentation-over-Histological-Images\imageExamples.npy')
+masks=np.load(r'C:\Users\ugur_\OneDrive\Masaüstü\Projects\Cancer-Detection-and-segmentation-over-Histological-Images\Cancer-Detection-and-segmentation-over-Histological-Images\maskExamples.npy')
 images=images.astype(int)
-print(images.shape)
-print(images[0].shape)
-print(images[1].shape)
-print(images[0][1].shape)
+masks=masks.astype(int)
+
+# imageExampels=[]
+# maskExampels=[]
+
+# for i in range(100):
+#     imageExampels.append(images[i])
+#     maskExampels.append(masks[i])
+
+# imageExampels=np.array(imageExampels)
+# maskExampels=np.array(maskExampels)
+# np.save('imageExamples', imageExampels)
+# np.save('maskExamples', maskExampels)
 
 
-plt.imshow(images[0])
-plt(masks[0])
-plt.show()
+# plt.imshow(images[0])
+# plt.imshow(masks[0])
+# plt(images[0])
+# plt(masks[0])
 
 
-# for i in range (100):
-#     plt.imshow(images[i])
-#     plt(masks[i])
-#     imagename='images'+str(i)+'.png'
-#     plt.savefig(imagename)
+
+for i in range (1):
+    plt.imshow(images[i])
+    plt.imshow(masks[i])
+    imagename='images'+str(i)+'.png'
+    plt.savefig(imagename)
     
-
-
-image1 = images[0]
 
 plt.show()
 
