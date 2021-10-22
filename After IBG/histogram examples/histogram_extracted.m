89158a17e1e1b0ac750213f9abe1cc9fa8991cc1
zovgu <ovgu1226@hotmail.com>
2021-10-23 01:51:34 +0300

    Update histogram_extracted.m

--- After IBG/histogram examples/histogram_extracted.m
+++ After IBG/histogram examples/histogram_extracted.m
@@ -1,18 +1,19 @@
-function y = histogram_extracted(im,name)
+function y = histogram_extracted(im,name) 
+% make a function with input rgb image and the name of the image for plot
     
-    % gray scale transition
-    gray_scaled = rgb2gray(im);
+    gray_scaled = rgb2gray(im); % gray scale transition
    
-    % my own histogram extracting method
-    
+    % my own histogram extracting method (ZOY)
     L = 256; % the gray scale length
-    h = zeros(1,L);  %
-    [r , c] = size(gray_scaled);
+    h = zeros(1,L);  % histogram array creation
+    [r , c] = size(gray_scaled); % size of the image extraction
 
-    for i = 1 : r
-        for j = 1 : c
-            px = gray_scaled(i, j);
-            h(px + 1) = h(px + 1) + 1;
+    for i = 1 : r % for loop for rows
+        for j = 1 : c % for loop for columns 
+
+            px = gray_scaled(i, j); % extracting of pixel value
+            h(px + 1) = h(px + 1) + 1; % filling the histogram array
+
         end
     end
 
@@ -20,27 +21,27 @@ function y = histogram_extracted(im,name)
     [counts, gLevels] = imhist(gray_scaled);
     
 
-    y = figure('Position', get(0, 'Screensize'));
-    sgtitle(name);
+    y = figure('Position', get(0, 'Screensize')); % make the plot full screen
+    sgtitle(name); % title of the plot
 
-    subplot(221); 
-    imshow(im); 
-    title("Imput Image");
+    subplot(221); % create subplot
+    imshow(im); % fill the first subplot
+    title("Imput Image"); % subplot title
 
     subplot(222); 
-    imshow(gray_scaled);
-    title("Gray Scale Image");
+    imshow(gray_scaled); % fill the second subplot
+    title("Gray Scale Image"); % subplot title
 
     subplot(223); 
-    bar(gLevels, counts); 
-    xlim([0, max(gLevels)]);
+    bar(gLevels, counts); % fill the third subplot
+    xlim([0, max(gLevels)]); % x and y axis limits
     ylim([0, max(counts)])
-    subtitle("Built-In Histogram Maker");
+    subtitle("Built-In Histogram Maker"); % subplot title
 
     subplot(224); 
-    stem(h,"*"); 
-    xlim([0, max(gLevels)]);
+    stem(h,"*"); % fill the last subplot
+    xlim([0, max(gLevels)]); % x and y axis limits
     ylim([0, max(counts)])
-    subtitle("My Histogram Maker");
+    subtitle("My Histogram Maker"); % subplot title
  
 end
