0bf8f245c53802e992bcbbaa02961d0478a2709c
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-04-02 12:56:08 +0300

    Camera Permission and Permission request added

--- AndroidCameraApp/app/src/main/java/com/example/microsphone/MainActivity.java
+++ AndroidCameraApp/app/src/main/java/com/example/microsphone/MainActivity.java
@@ -8,16 +8,20 @@ import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -47,20 +51,20 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
 
+    private static final int REQUEST_CAMERA_PERMISSION_RESULT = 0;
     private TextureView mTextureView;
     public static ArrayList<String> logList = new ArrayList<>();
-    public  static String text2 = "App Started";
+    public static String text2 = "App Started";
     public static int i;
 
-    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener()
-    {
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
         @Override
-        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height)
-        {
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
             Toast.makeText(MainActivity.this, "Texture view is available", Toast.LENGTH_SHORT).show();
 
             try {
-                setupCamera(width,height);
+                setupCamera(width, height);
+                connectCamera();
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
@@ -94,40 +98,40 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         public void onOpened(@NonNull CameraDevice camera)
         {
             mCameraDevice = camera;
+            Toast.makeText(getApplicationContext(),"Camera connection is succesfull",Toast.LENGTH_SHORT).show();
+            log("Camera is connected.....");
 
 
         }
 
         @Override
-        public void onDisconnected(@NonNull CameraDevice camera)
-        {
+        public void onDisconnected(@NonNull CameraDevice camera) {
             camera.close();
             mCameraDevice = null;
         }
 
         @Override
-        public void onError(@NonNull CameraDevice camera, int error)
-        {
+        public void onError(@NonNull CameraDevice camera, int error) {
             camera.close();
             mCameraDevice = null;
 
         }
     };
 
-    private  String mcameraID;
+    private String mcameraID;
     private Size mPreviewSize;
     private HandlerThread mBackgroundHandlerThread;
     private Handler mBackgroundHandler;
     private static SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static
-    {
-        ORIENTATIONS.append(Surface.ROTATION_0,0);
-        ORIENTATIONS.append(Surface.ROTATION_90,90);
-        ORIENTATIONS.append(Surface.ROTATION_180,180);
-        ORIENTATIONS.append(Surface.ROTATION_270,270);
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
     }
 
-    private static class CompareSizeByArea implements Comparator<Size>{
+    private static class CompareSizeByArea implements Comparator<Size> {
         @Override
         public int compare(Size lhs, Size rhs) {
             return (int) (Long.signum((long) lhs.getWidth() * lhs.getHeight()) / (long) rhs.getWidth() * rhs.getHeight());
@@ -136,11 +140,10 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
 
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus)
-    {
+    public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         View decorView = getWindow().getDecorView();
-        if(hasFocus){
+        if (hasFocus) {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -151,17 +154,14 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         }
     }
 
-    private void closeCamera()
-    {
-        if(mCameraDevice != null)
-        {
+    private void closeCamera() {
+        if (mCameraDevice != null) {
             mCameraDevice.close();
             mCameraDevice = null;
         }
     }
 
-    private void startBackgroundThread()
-    {
+    private void startBackgroundThread() {
         mBackgroundHandlerThread = new HandlerThread("camea2VideoImage");
         mBackgroundHandlerThread.start();
         log("Thread Started");
@@ -176,33 +176,26 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         log("Thread Stopped");
     }
 
-    private static int sensorTodeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation)
-    {
+    private static int sensorTodeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation) {
         int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
         deviceOrientation = ORIENTATIONS.get(deviceOrientation);
-        return (sensorOrientation + deviceOrientation +360) % 360;
+        return (sensorOrientation + deviceOrientation + 360) % 360;
 
     }
 
-    private static Size optimalSize(Size[] choices, int width, int height)
-    {
+    private static Size optimalSize(Size[] choices, int width, int height) {
         List<Size> bigEnough = new ArrayList<Size>();
-        for(Size option : choices)
-        {
-            if(option.getHeight() == option.getWidth() * height / width &&
-            option.getWidth() >= width && option.getHeight() >= height)
-            {
+        for (Size option : choices) {
+            if (option.getHeight() == option.getWidth() * height / width &&
+                    option.getWidth() >= width && option.getHeight() >= height) {
                 bigEnough.add(option);
             }
         }
 
-        if(bigEnough.size() >0)
-        {
+        if (bigEnough.size() > 0) {
             return Collections.min(bigEnough, new CompareSizeByArea());
-        }
-        else
-        {
-           return choices[0];
+        } else {
+            return choices[0];
         }
     }
 
@@ -210,13 +203,11 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
 
-        for(String cameraID : cameraManager.getCameraIdList())
-        {
+        for (String cameraID : cameraManager.getCameraIdList()) {
             CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraID);
 
-            if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING)==
-            CameraCharacteristics.LENS_FACING_BACK)
-            {
+            if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) ==
+                    CameraCharacteristics.LENS_FACING_BACK) {
                 continue;
             }
 
@@ -225,29 +216,55 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
 
             int deviceOrientation = getWindowManager().getDefaultDisplay().getRotation();
             int totalRotation = sensorTodeviceRotation(cameraCharacteristics, deviceOrientation);
-            boolean swapRotation = totalRotation == 90 || totalRotation ==270;
+            boolean swapRotation = totalRotation == 90 || totalRotation == 270;
             int rotateWidth = width;
             int rotateHeight = height;
 
-            if(swapRotation)
-            {
+            if (swapRotation) {
                 rotateWidth = height;
                 rotateHeight = width;
             }
 
-            mPreviewSize = optimalSize(map.getOutputSizes(SurfaceTexture.class),rotateWidth, rotateHeight);
+            mPreviewSize = optimalSize(map.getOutputSizes(SurfaceTexture.class), rotateWidth, rotateHeight);
 
             mcameraID = cameraID;
 
-            log("Camera ID is: "+cameraID);
-            log("Camera ID is: "+cameraID);
-
-
-
-
+            log("Camera ID is: " + cameraID);
 
             return;
 
+        }
+    }
+
+    private void connectCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==
+                PackageManager.PERMISSION_GRANTED)
+                {
+                    cameraManager.openCamera(mcameraID, mCameraDeviceStateCallback, mBackgroundHandler);
+
+                }
+                else
+                {
+                    if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA))
+                    {
+                        Toast.makeText(this,"MicrosPhone requires to access to Camera",Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION_RESULT);
+
+                }
+
+            }
+            else {
+                cameraManager.openCamera(mcameraID, mCameraDeviceStateCallback, mBackgroundHandler);
+
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
@@ -289,6 +306,7 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         {
             try {
                 setupCamera(mTextureView.getWidth(),mTextureView.getHeight());
+                connectCamera();
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
@@ -300,6 +318,20 @@ public class MainActivity<stastic> extends AppCompatActivity implements View.OnC
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CAMERA_PERMISSION_RESULT)
+        {
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(getApplicationContext(),"Application will not run without camera services",Toast.LENGTH_SHORT).show();
+                log("Application requieres camera permission");
+            }
+        }
+    }
+
     protected void onPause()
     {
         closeCamera();
