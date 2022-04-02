c2858f9efc9759990e9d2b9475eca47ae637d77b
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-04-02 06:20:25 +0300

    updated

--- PythonDesktopApp/PythonSocket/client.py
+++ PythonDesktopApp/PythonSocket/client.py
@@ -8,16 +8,12 @@ Created on Sat Oct 23 16:35:55 2021
 
 import socket, cv2, pickle, struct
 import cv2
-import gi
+ 
 import numpy as np
-
-gi.require_version('Gst', '1.0')
-from gi.repository import Gst
+ 
 
 
 #creating the socket
-
-
 
 
 
@@ -34,120 +30,12 @@ class Video():
     """
     
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host_ip = "10.100.153.85"
-    port = 5600
+    host_ip = "127.0.0.1"
+    port = 9999
 
     client_socket.connect((host_ip, port)) # a tuple
 
-    
-
-    def __init__(self, port=5600):
-        """Summary
-        Args:
-            port (int, optional): UDP port
-        """
-
-        Gst.init(None)
-
-        self.port = port
-        self._frame = None
-
-        # [Software component diagram](https://www.ardusub.com/software/components.html)
-        # UDP video stream (:5600)
-        self.video_source = 'udpsrc port={}'.format(self.port)
-        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
-        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
-        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
-        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
-        self.video_decode = \
-            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
-        # Create a sink to get data
-        self.video_sink_conf = \
-            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
-
-        self.video_pipe = None
-        self.video_sink = None
-
-        self.run()
-
-    def start_gst(self, config=None):
-        """ Start gstreamer pipeline and sink
-        Pipeline description list e.g:
-            [
-                'videotestsrc ! decodebin', \
-                '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                '! appsink'
-            ]
-        Args:
-            config (list, optional): Gstreamer pileline description list
-        """
-
-        if not config:
-            config = \
-                [
-                    'videotestsrc ! decodebin',
-                    '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                    '! appsink'
-                ]
-
-        command = ' '.join(config)
-        self.video_pipe = Gst.parse_launch(command)
-        self.video_pipe.set_state(Gst.State.PLAYING)
-        self.video_sink = self.video_pipe.get_by_name('appsink0')
-
-    @staticmethod
-    def gst_to_opencv(sample):
-        """Transform byte array into np array
-        Args:
-            sample (TYPE): Description
-        Returns:
-            TYPE: Description
-        """
-        buf = sample.get_buffer()
-        caps = sample.get_caps()
-        array = np.ndarray(
-            (
-                caps.get_structure(0).get_value('height'),
-                caps.get_structure(0).get_value('width'),
-                3
-            ),
-            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
-        return array
-
-    def frame(self):
-        """ Get Frame
-        Returns:
-            iterable: bool and image frame, cap.read() output
-        """
-        return self._frame
-
-    def frame_available(self):
-        """Check if frame is available
-        Returns:
-            bool: true if frame is available
-        """
-        return type(self._frame) != type(None)
-
-    def run(self):
-        """ Get frame to update _frame
-        """
-
-        self.start_gst(
-            [
-                self.video_source,
-                self.video_codec,
-                self.video_decode,
-                self.video_sink_conf
-            ])
-
-        self.video_sink.connect('new-sample', self.callback)
-
-    def callback(self, sink):
-        sample = sink.emit('pull-sample')
-        new_frame = self.gst_to_opencv(sample)
-        self._frame = new_frame
-
-        return Gst.FlowReturn.OK
+   
 
 
 if __name__ == '__main__':
