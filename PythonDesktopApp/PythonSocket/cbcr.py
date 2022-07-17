c5b85bce9189491c1c23361ec76ef0d87082bd6f
zeynepbetul <z.beetulkaya@gmail.com>
2022-07-17 22:54:00 +0300

    Add cbcr tansformation function

---
+++ PythonDesktopApp/PythonSocket/cbcr.py
@@ -0,0 +1,12 @@
+import cv2
+
+def cbcr_transform(image):
+    YCrCb = cv2.cvtColor(image, cv2.cv.CV_BGR2YCrCb)
+    # Luminance Y
+    Y = YCrCb[:, :, 0]
+    # Chrominance Cr
+    Cr = YCrCb[:, :, 1]
+    # Chrominance Cb
+    Cb = YCrCb[:, :, 2]
+
+    return Cr, Cb
